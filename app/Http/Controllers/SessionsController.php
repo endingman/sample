2361dda754f05ff1174bb9489222f401c69de5a5
endingman <631925002@qq.com>
2017-03-16 12:18:58 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    public function __construct($value = '')
+    {
+        $this->middleware('guest', ['only' => ['create']]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -29,7 +33,8 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
+// redirect() 实例提供了一个 intended 方法，该方法可将页面重定向到上一次请求尝试访问的页面上，并接收一个默认跳转地址参数，当上一次请求记录为空时，跳转到默认地址上
             //  Laravel 提供的 Auth::user() 方法来获取当前登录用户的信息，并将数据传送给路由。
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,10 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+
+    public function __construct($value = '')
+    {
+        $this->middleware('auth', [
+            'only' => ['edit', 'update', 'destroy'],
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create'],
+        ]);
+        // __construct 是 PHP 的构造器方法，当一个类对象被创建之前该方法将会被调用。我们在 __construct 方法中调用了 middleware 方法，该方法接收两个参数，第一个为中间件的名称，第二个为要进行过滤的动作。我们通过 only 方法来为 指定动作 使用 Auth 中间件进行过滤。注意：两个_
+    }
+
     public function create()
     {
         return view('users.create');
@@ -18,6 +32,7 @@ class UsersController extends Controller
         $user = User::findOrFail($id);
         return view('users.show', compact('user'));
     }
+
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -34,4 +49,47 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'password' => 'confirmed|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        $data         = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $id);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10); //数据分页，laravel自带，10表示每页10条数据
+        return view('users.index', compact('users'));
+        # code...
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 }
